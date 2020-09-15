612503e17726e079df8a4c1a72a56b08d7904ea1
barreto <60680794+barreto2001@users.noreply.github.com>
2020-09-15 16:44:38 -0500

    Decrecimietno de lorem

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,16 +14,7 @@
 </head>
 <body>
     <div class="container">
-        <div class="row"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur eius magnam in sequi corporis dignissimos fugiat quibusdam ea esse quidem, suscipit eum aut accusamus consequatur, explicabo hic enim! Veritatis, eveniet.
-        Praesentium alias tempore itaque? Iste dolorum beatae, at quis cumque ut magnam illo, cum porro ex veniam et dolore repudiandae officiis facere. Impedit soluta voluptatem laborum velit quibusdam aliquid fugit?
-        Hic nesciunt in omnis! Sint nam alias hic amet. A at deleniti excepturi aut ratione adipisci, quam, et magnam, in sit corporis illo voluptate modi quaerat! Laudantium quibusdam mollitia voluptas.
-        Animi obcaecati cupiditate atque suscipit voluptas harum recusandae minima. Quaerat et provident adipisci aliquid debitis impedit dolore cumque doloribus, voluptas minus dolorum quibusdam quisquam nesciunt perferendis facilis, quidem ipsa sapiente.
-        Vero explicabo magni perspiciatis quasi amet veritatis, laudantium placeat magnam dicta cupiditate laborum, quas iusto maxime ad omnis unde repellendus. Perferendis magnam minus accusamus ad beatae. Nam voluptatibus vero id!
-        Ipsa distinctio, nobis, harum necessitatibus, corporis hic iure cupiditate sit eveniet sapiente cumque repellendus dolore quia ducimus dolorum quasi. Omnis expedita eius soluta reprehenderit consectetur odit inventore repellendus, minima amet?
-        Dolor repellendus quaerat eligendi sed recusandae similique ad omnis dignissimos totam tenetur facere laudantium eos dicta fugit, adipisci quia minima aliquam numquam porro voluptas temporibus commodi delectus! Culpa, ea cumque?
-        Deserunt mollitia itaque quos, impedit molestiae deleniti at, vero perferendis molestias dicta asperiores aliquid incidunt possimus delectus vitae, commodi voluptatem. Debitis facere mollitia dolores? Nisi aperiam ipsam quo dolor non.
-        Tempore alias possimus suscipit magni quas dolor sit eligendi, eaque pariatur quae modi nam voluptatum reiciendis est dolores ex quaerat aliquam exercitationem saepe dolorem, sunt laudantium voluptates totam. Magni, unde?
-        Ut, ipsum laudantium earum, deserunt corrupti magni architecto modi delectus ipsam natus maiores quasi ducimus quibusdam id aliquam voluptate recusandae exercitationem unde sit commodi! Nihil, dolore. Aperiam similique deleniti mollitia?</p></div>
+        <div class="row"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur eius magnam in sequi corporis dignissimos fugiat quibusdam ea esse quidem, suscipit eum aut accusamus consequatur, explicabo hic enim! Veritatis, e</p></div>
     </div>
 </body>
 </html>
